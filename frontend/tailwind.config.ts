c3700b4f824d03fb99f1ed32e73d7dc1044c2426
Soham Kundu <sohamputukundu@gmail.com>
2026-07-18 16:43:07 -0400

    added postcss.config.js file

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,12 +2,6 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    // Include prefixed paths to account for your monorepo folder structure
-    "./frontend/src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./frontend/src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./frontend/src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    
-    // Keep these just in case Vercel builds relative to the frontend directory root
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
