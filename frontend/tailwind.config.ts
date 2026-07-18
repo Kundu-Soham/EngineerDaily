d400565a25e28781d1c101f84a969a1172f4ad12
Soham Kundu <sohamputukundu@gmail.com>
2026-07-18 16:36:55 -0400

    Trying styling fix #3

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,17 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    // Include prefixed paths to account for your monorepo folder structure
+    "./frontend/src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./frontend/src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./frontend/src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    
+    // Keep these just in case Vercel builds relative to the frontend directory root
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
